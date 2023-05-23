77505bfddb28cefec297d4e78536160b60633aca
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2023-05-23 10:09:18 +0200

    Smll changes to api client

--- src/APIClient.js
+++ src/APIClient.js
@@ -10,7 +10,7 @@ export default class API {
      * @param {Function} onLoad Function which runs when the levels have loaded. Receives level info as arg
      */
     static getMainLevelsInfo(onLoad) {
-        XMLGet("get_main_levels_info.php", (response) => {
+        xmlGet("get_main_levels_info", null, (response) => {
             const levelInfoList = [];
             response.forEach(leveldata => {
                 let info;
@@ -23,18 +23,18 @@ export default class API {
     }
 
     /**
-     * Get complete level info and content from level ID
-     * @param {number} levelId
+     * Get complete level info and content from level ID (Main levels only)
+     * @param {number} id ID of level.
      * @param {Function} onLoad Function which runs when content has loaded. Receives level info as arg
      */
-    static getMainLevelContent(levelId, onLoad) {
-        XMLGet(`get_main_level_from_id.php?id=${levelId}`, (response) => {
+    static getMainLevelContent(id, onLoad) {
+        xmlGet(`get_main_level_from_id`, {id: id}, (response) => {
             onLoad(response.data, response.info);
         });
     }
 
     static getCreatedLevelsInfo(onLoad) {
-        XMLGet("get_created_levels_info.php", (response) => {
+        xmlGet("get_created_levels_info", null, (response) => {
             response = response.map(leveldata => {
                 return tryToParse(leveldata);
             });
@@ -43,22 +43,62 @@ export default class API {
     }
 }
 
-function XMLGet(method, onLoad) {
+/**
+ * Helper function for doing an XML GET-request.
+ * @param {string} method Name of method (without ".php")
+ * @param {object} data Object containing the data included in the url.
+ * @param {Function} onLoad Function
+ */
+function xmlGet(method, data, onLoad) {
     const xml = new XMLHttpRequest();
-    const url = API_PATH + method;
+    const url = formatRequestURL(method, data);    
+
     xml.open("get", url, true);
 
     xml.onreadystatechange = function() {
-        if(xml.readyState == 4 && xml.status == 200) {
+        if(xmlReady(xml)) {
             let response = tryToParse(this.responseText, true);
             onLoad(response);
         }
     };
 
-
     xml.send();
 }
 
+function xmlReady(xml) {
+    return (xml.readyState == 4 && xml.status == 200);
+}
+
+/**
+ * Format a request url with data.
+ * Ex: method = "example" & data = {foo: "bar", fizz: "buzz"} 
+ * returns => example.php?foo=bar&fizz=buzz
+ * @param {string} methodName Name of method (not including ".php");
+ * @param {object?} data Method data
+ * @returns {string}
+ */
+function formatRequestURL(methodName, data) {
+    let url = API_PATH + methodName + ".php";
+
+    if(!data || typeof data !== "object") {
+        return url;
+    }
+
+    let urlData = [];
+    for(const key in data) {
+        urlData.push(`${key}=${data[key]}`);
+    }
+
+    return url + "?" + urlData.join("&");
+}
+
+/**
+ * Try to parse a JSON string, and return the result.
+ * If parsing fails, return the passed string, or throw an error.
+ * @param {string} json JSON string
+ * @param {boolean} errorOnFail If true, throw an error if parsing fails.
+ * @returns {string}
+ */
 function tryToParse(json, errorOnFail = false) {
     try { 
         return JSON.parse(json);
